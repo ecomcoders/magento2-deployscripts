388ec9ae7819579065162771f200aa6ea2e279d1
Christian Grugel <christian@grugel.net>
2017-06-30 15:27:46 +0200

    Add missing slash

--- ecomcoders-build.sh
+++ ecomcoders-build.sh
@@ -40,7 +40,7 @@ create_package()
     echo "Creating package '${PACKAGE_NAME}'"
     echo "----------------------------------------------------"
     tar -czf "${PACKAGE_PATH}${PACKAGE_NAME}" \
-        --exclude-vcs
+        --exclude-vcs \
         --exclude='.*auth\.json' \
         --exclude='.*composer\.json' .
 }
