28ad37ab1b98276178b80e7f5212a12e90052ba8
Christian Grugel <christian.grugel@ecomcoders.com>
2021-03-20 15:58:59 +0100

    Show upgradable packages

--- ecomcoders-update-notifier.sh
+++ ecomcoders-update-notifier.sh
@@ -5,3 +5,5 @@ echo -e 'Subject: Update Manager Report\n\n'
 cat /var/lib/update-notifier/updates-available
 LAST_UPDATES=$(date -r /var/lib/update-notifier/updates-available)
 echo "Last updated: $LAST_UPDATES"
+
+apt list --upgradable
