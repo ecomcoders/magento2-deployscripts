202f53dd1e95778f9ad4922ff1d9754311a10425
Christian Grugel <christian.grugel@ecomcoders.com>
2021-03-20 11:26:23 +0100

    Add simple update manager report

---
+++ ecomcoders-update-notifier.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo -e 'Subject: Update Manager Report\n\n'
+cat /var/lib/update-notifier/updates-available
+LAST_UPDATES=$(date -r /var/lib/update-notifier/updates-available)
+echo 'Last updated: $LAST_UPDATES'
