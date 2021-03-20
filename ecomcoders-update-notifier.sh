b44124d83523dfb9ffb6a8b3fcd6c0374328b69a
Christian Grugel <christian.grugel@ecomcoders.com>
2021-03-20 14:46:18 +0100

    Fix quotes

--- ecomcoders-update-notifier.sh
+++ ecomcoders-update-notifier.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 echo -e 'Subject: Update Manager Report\n\n'
 cat /var/lib/update-notifier/updates-available
 LAST_UPDATES=$(date -r /var/lib/update-notifier/updates-available)
-echo 'Last updated: $LAST_UPDATES'
+echo "Last updated: $LAST_UPDATES"
