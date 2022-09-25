ebe25d418d1cdb749c8c1ecd165db8b2656d6cb7
Christian Grugel <christian.grugel@ecomcoders.com>
2022-09-25 08:19:17 +0200

    Replace "setup:upgrade" by "setup:db-schema:upgrade & setup:db-data:upgrade"

--- ecomcoders-install.sh
+++ ecomcoders-install.sh
@@ -72,9 +72,8 @@ make_magento_production_ready()
 {
     STATIC_CONTENT_DEPLOY_PARAMS=$($EST STATIC_CONTENT_DEPLOY_PARAMS)
 
-    $MAGENTO_CLI indexer:status
-    $MAGENTO_CLI indexer:reindex
-    $MAGENTO_CLI -vvv setup:upgrade
+    $MAGENTO_CLI setup:db-schema:upgrade
+    $MAGENTO_CLI setup:db-data:upgrade
     $MAGENTO_CLI setup:di:compile
     $MAGENTO_CLI deploy:mode:set --skip-compilation production
     $MAGENTO_CLI setup:static-content:deploy $STATIC_CONTENT_DEPLOY_PARAMS
